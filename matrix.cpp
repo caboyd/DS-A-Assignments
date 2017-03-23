a5961ab6789405982fcc4c9bb0fb5cb3602a555c
Chris Boyd <boyd111c@gmail.com>
2017-03-22 21:23:42 -0600

    Added create matrix, so all 3 matrix work

--- matrix.cpp
+++ matrix.cpp
@@ -10,12 +10,16 @@ This program creates child threads
 
 using namespace  std;
 
-int** readMatrixFromFile(std::string fileName);
+int** readMatrixFromFile(std::string fileName, int &row, int &col);
 void parseRowAndColumn(ifstream &input, int &row, int &col);
 void parseMatrix(ifstream &input, int** matrix, const int row, const int col);
+int ** createMatrix(const int row, const int col);
 
-int n;
-
+int ** a;
+int ** b;
+int ** c;
+//c matrix will be of dimensions m x r
+int m,r;
 
 //Prototype thread functions as C functions
 extern "C" {
@@ -25,20 +29,38 @@ extern "C" {
 
 int main(int argc, char *argv[])
 {
-	int error;
-	int i;
-
-	int **a = readMatrixFromFile("a1.txt");
-	a = a;
+	int row, col;
+	//a matrix m x n
+	a = readMatrixFromFile("a1.txt", row ,col);
+	m = row;
+	//b matrix n x r
+	b = readMatrixFromFile("a2.txt", row, col);
+	r = col;
+	//c matrix will be of dimensions m x r
+	c = createMatrix(m, r);
 
 	return 0;
 }
 
-int** readMatrixFromFile(string fileName)
+int** createMatrix(const int row, const int col)
+{
+	int **matrix;
+	matrix = new int*[row];
+	for(int i = 0; i < row; i++)
+	{
+		matrix[i] = new int[col];
+		for(int j = 0;j < col; j++)
+		{
+			matrix[i][j] = 0;
+		}
+	}
+	return matrix;
+}
+
+int** readMatrixFromFile(string fileName, int &row, int &col)
 {
 	ifstream input(fileName.c_str());
 	string s;
-	int row, col;
 	int ** matrix;
 	parseRowAndColumn(input, row, col);
 
@@ -73,6 +95,7 @@ void parseMatrix(ifstream &input, int** matrix, const int maxRow, const int maxC
 
 void parseRowAndColumn(ifstream &input, int &row, int &col)
 {
+	//string to hold row and col with extra junk
 	string srow, scol;
 	//junk the first line
 	getline(input, srow);
