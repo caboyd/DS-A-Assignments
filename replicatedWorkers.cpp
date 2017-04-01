d952e1df558e9412ba7333ed4ec0d4c136d86633
Chris Boyd <boyd111c@gmail.com>
2017-04-01 17:24:10 -0600

    a

--- replicatedWorkers.cpp
+++ replicatedWorkers.cpp
@@ -23,7 +23,7 @@ pthread_mutex_t output_lock;
 const int NO_OF_WORKERS = 5;
 const int NO_OF_WORK_POOLS = 3;
 //total threads
-const int n = NO_OF_WORKERS * NO_OF_WORK_POOLS;
+const int n = NO_OF_WORKERS * NO_OF_WORK_POOLS + 1;
 
 int t[NO_OF_WORK_POOLS + 1];
 sem_t s[NO_OF_WORK_POOLS + 1]; //t's lock
